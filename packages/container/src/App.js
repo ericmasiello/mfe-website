b394d3ddbc7cba7e2be94cbb0ee997aa6c215234
Eric Masiello <eric.j.masiello@gmail.com>
2020-12-23 12:13:59 -0500

    namespace MUI production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  // Replaces default "jss" production classname prefix with "co"
+  // This will avoid collisions with other apps
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
