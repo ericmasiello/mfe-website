aabd0725ad56d9bab17a02e650d2e3c89bf5235c
Eric Masiello <eric.j.masiello@gmail.com>
2020-12-24 06:12:03 -0500

    setup history communication bdtween container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,27 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 // "'marketing'" has to match the key found in container's ModuleFederationPlugin remotes
 // "/MarketingApp" has to match whatever is exposed via the marketing apps ModuleFederationPlugin "exposes" key
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
+
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        // help avoid infinite loops by verifying
+        // we are actaully changing the path
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
+
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    // parent app will call this whenever it navigates
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation,
@@ -13,7 +30,13 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {
+      // use browser history when in isolation mode
+      defaultHistory: createBrowserHistory(),
+      onNavigate: ({ pathname }) => {
+        console.log('called onNavigation with:', pathname);
+      },
+    });
   }
 }
 
